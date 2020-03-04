5c3adb9d2fc93b703c77263043929de1a8b28281
montoyamoraga <montoyamoraga@gmail.com>
2020-03-04 01:30:06 -0500

    agregar borrador ejemplo02

--- actividades/actividad-02/js/sketch.js
+++ actividades/actividad-02/js/sketch.js
@@ -1,19 +1,36 @@
+
+let lienzo;
+
+let osc;
+
+let estaPrendido = false;
+
 // setup() ocure una vez al inicio
 function setup() {
   // crear lienzo, 800px ancho, 600 px alto, 2D
-  createCanvas(800, 600);
+  lienzo = createCanvas(800, 600);
+
+  // agregar respuesta asíncrona
+  // cuando se presiona el ratón, se ejecuta prenderOscilador()
+  lienzo.mousePressed(prenderOsciladores);
 
   // fondo (rojo, verde, azul), valores 0 a 255
   background(0, 255, 0);
   
   // no dibujar contorno de figuras geometricas
   noStroke();
+
+  // crear nuevo oscilador sinusoidal
+  osc = new p5.Oscillator("sine");
 }
 
 // draw() ocurre despues de setup(), en bucle
 function draw() {
   // dibujar elipse en la posicion del raton, 20px altura, 30px ancho
   ellipse(mouseX, mouseY, 20, 30);
+
+  osc.freq(map(mouseX, 0, width, 300, 450));
+  osc.amp(map(mouseY, 0, height, 1.0, 0.0));
 }
 
 // mouseClicked() ocurre cuando se hace click
@@ -23,4 +40,12 @@ function mouseClicked() {
   background(random(255), random(255), random(255));
   // color aleatorio de relleno
   fill(random(255), random(255), random(255));
+
+}
+
+// función ejecutada cuando se hace click en el lienzo
+function prenderOsciladores() {
+  if (!estaPrendido) {
+    osc.start();
+  }
 }
