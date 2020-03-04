3a5b4d80a17154d7733298c75028837c94c1f199
montoyamoraga <montoyamoraga@gmail.com>
2020-03-04 00:02:14 -0500

    agregar borrador de info

--- actividades/actividad-04/js/sketch.js
+++ actividades/actividad-04/js/sketch.js
@@ -1,3 +1,23 @@
+// actividad 03
+
+// declarar variables globales
+
+// variable info para almacenar el archivo json
+let info = null;
+
+// variable para almacenar las llaves del archivo json
+let llaves = null
+
+// preload() es una función opcional de p5.js
+// preload() espera que su ejecución termine antes de avanzar a setup()
+// preload(), si existe, ocurre una vez justo antes de setup()
+// sirve para cargar archivos y esperar a que sean cargados
+function preload() {
+  // cargar archivo json
+  info = loadJSON("./data/info.json");
+}
+
+// setup() es 
 function setup() {
   // escribir codigo aca
 }
